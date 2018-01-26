baa4a795b8711b2b56477dcdf16d00843ae0511d
Jacob Reckhard <jacobreckhard@gmail.com>
2018-01-26 12:47:07 -0700

    added file reading functions

--- src/nativefuncs.py
+++ src/nativefuncs.py
@@ -1,6 +1,7 @@
 import interpreter
 from tokens import Token
 import time
+import os
 
 
 def addNatives(enviro):
@@ -17,4 +18,22 @@ def addNatives(enviro):
     enviro.define(Token(None, "input", None, None), interpreter.Callable(
         lambda x, y: input(),           # The function call
         lambda: 0                       # The number of arguments
+    ))
+
+    enviro.define(Token(None, "read", None, None), interpreter.Callable(
+        # The function call
+        lambda x, y: open(y[0], 'r').read(),
+        lambda: 1                       # The number of arguments
+    ))
+
+    enviro.define(Token(None, "write", None, None), interpreter.Callable(
+        lambda x, y: os.remove(y[0]) if y[1] is None else open(
+            y[0], 'w+').write(y[1]),    # The function call
+        lambda: 2                       # The number of arguments
+    ))
+
+    enviro.define(Token(None, "append", None, None), interpreter.Callable(
+
+        lambda x, y: open(y[0], 'a+').write(y[1]),  # The function call
+        lambda: 2                       # The number of arguments
     ))
